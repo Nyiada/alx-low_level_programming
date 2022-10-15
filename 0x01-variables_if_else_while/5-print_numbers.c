348078d452a3e2b8e76c28d8b3f9260e73c7d87b
Nyiada <nyiadantj@gmail.com>
2022-10-15 09:58:57 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main - Short description, single line
+  * a blank line
+  * Description: Longer description of the function)?
+  * Return: Description of the returned value
+  */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
