755ab38166695734f9a039edbb98e74d1d517fbc
Nyiada <nyiadantj@gmail.com>
2022-10-15 10:06:57 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-this is the main
+ * a blank line
+ * Description: this is the description of function
+ * Return:description of the return value
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
