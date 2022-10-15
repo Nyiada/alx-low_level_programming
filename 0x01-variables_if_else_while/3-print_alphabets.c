1b7ca4e6cbd39ade96ebe3a21fbd66e8afc65747
Nyiada <nyiadantj@gmail.com>
2022-10-15 08:37:22 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * a blank line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+}
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
