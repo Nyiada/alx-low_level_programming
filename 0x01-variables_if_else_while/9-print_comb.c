0c03854624b003395a55af24b429f430a0168ff1
Nyiada <nyiadantj@gmail.com>
2022-10-15 13:50:24 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a long description of the function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
