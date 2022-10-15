6fa175a13a08d86b91d617e815e405e8f60bc33e
Nyiada <nyiadantj@gmail.com>
2022-10-15 07:58:54 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-Short description, single line
+ * a blank line
+ * Description: Longer description of the function)?
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
